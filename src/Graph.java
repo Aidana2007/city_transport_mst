976c098b58c07203ec8869c2d8b49042d7fc6e6b
Aidana2007 <maksutova2007@inbox.ru>
2025-10-26 13:36:04 +0500

    created graph and edge classes

---
+++ src/Graph.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.util.List;
+
+public class Graph {
+    private int id;
+    private List<String> nodes;
+    private List<Edge> edges;
+
+    public Graph() {}
+
+    public Graph(int id, List<String> nodes, List<Edge> edges) {
+        this.id = id;
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public List<String> getNodes() { return nodes; }
+    public void setNodes(List<String> nodes) { this.nodes = nodes; }
+    public List<Edge> getEdges() { return edges; }
+    public void setEdges(List<Edge> edges) { this.edges = edges; }
+
+    public void printGraph() {
+        System.out.println("Graph ID: " + id);
+        System.out.println("Nodes: " + nodes);
+        System.out.println("Edges:");
+        for (Edge e : edges) {
+            System.out.println("  " + e);
+        }
+    }
+}
